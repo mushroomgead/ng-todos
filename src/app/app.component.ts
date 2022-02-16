1f88bfbedf5b3b90b22eab82cb67c4f9fb169f0b
Kate <pohnsawan.k@gmail.com>
2022-02-16 22:40:01 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
+type TodoItem = { title: string }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-todos';
+  public items: TodoItem[] = [{
+    title: 'hello no1',
+  }, {
+    title: 'hello no2',
+  }];
+  public todos: TodoItem[] = [];
+
+  @ViewChild('todoInput') public todoInput!: ElementRef;
+
+  public addTodo(text: string): void {
+    this.todos.push({ title: text })
+    this.todoInput.nativeElement.value = '';
+  }
+
+  public deleteTodo(i: number): void {
+    this.todos = this.todos.filter((_, index) => i !== index)
+  }
 }
